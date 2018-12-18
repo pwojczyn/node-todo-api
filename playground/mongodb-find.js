e576c2822ddbb28eadacfc1e63006bf2dcc311bf
pwojczyn <pwojczyn@gmail.com>
2018-12-18 16:04:17 +0100

    Add delete script

--- playground/mongodb-find.js
+++ playground/mongodb-find.js
@@ -1,8 +1,5 @@
 // const MongoClient = require('mongodb').MongoClient;
-const {
-  MongoClient,
-  ObjectID
-} = require('mongodb');
+const {MongoClient, ObjectID} = require('mongodb');
 
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   if (err) {
@@ -19,17 +16,15 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   //   console.log('Unable to fetch todos', err);
   // });
 
-  db.collection('Todos').find().count().then((count) => {
-    console.log(`Todos count: ${count}`);
-  }, (err) => {
-    console.log('Unable to fetch todos', err);
-  });
-
-  // db.collection('Users').find({
-  //   name: 'Andrew'
-  // }).toArray().then((docs) => {
-  //   console.log(JSON.stringify(docs, undefined, 2));
+  // db.collection('Todos').find().count().then((count) => {
+  //   console.log(`Todos count: ${count}`);
+  // }, (err) => {
+  //   console.log('Unable to fetch todos', err);
   // });
 
-  db.close();
+  db.collection('Users').find({name: 'Andrew'}).toArray().then((docs) => {
+    console.log(JSON.stringify(docs, undefined, 2));
+  });
+
+  // db.close();
 });
